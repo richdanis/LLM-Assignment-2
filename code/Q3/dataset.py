ed9743e593ef4e4049af2437d201e7d08a7fc18a
Richard Danis <rdanis@ethz.ch>
2023-06-03 18:13:09 +0200

    Optimized dataset class

--- code/Q3/dataset.py
+++ code/Q3/dataset.py
@@ -27,25 +27,21 @@ class GSMDataset(th.utils.data.Dataset):
         self.ans = ds['answer']
         self.qns = [qn for qn in self.qns]
         self.ans = [ans for ans in self.ans]
-        self.qns = tokenizer(self.qns, padding=False)
+        self.qns = tokenizer(self.qns, padding='longest', return_tensors="pt")
         self.ans = tokenizer(self.ans, padding=False)
-        self.q_max_len = max([len(qn) for qn in self.qns["input_ids"]])
-        self.a_max_len = max([len(ans) for ans in self.ans["input_ids"]])
+        self.a_max_len = max([len(an) for an in self.ans["input_ids"]])
 
     def __len__(self):
         return len(self.qns["input_ids"])
 
     def __getitem__(self, idx):
-        input_ids = self.qns["input_ids"][idx]
-        padding = [0] * (self.q_max_len - len(input_ids))
-        attention_mask = [1] * len(input_ids) + padding
-        input_ids = input_ids + padding
 
         # important, padding is not ignored for labels, -100 is the ignore index
         padding = [-100] * (self.a_max_len - len(self.ans["input_ids"][idx]))
         labels = self.ans["input_ids"][idx] + padding
 
-        input_ids = th.tensor(input_ids)
         labels = th.tensor(labels)
-        attention_mask = th.tensor(attention_mask)
-        return dict(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+
+        return dict(input_ids=self.qns["input_ids"][idx], \
+                     attention_mask=self.qns["attention_mask"][idx], \
+                        labels=labels)
