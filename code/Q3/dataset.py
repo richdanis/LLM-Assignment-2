c0c54222893d2db39fa576d9343d47d7e9f19c3d
Richard Danis <rdanis@ethz.ch>
2023-06-07 16:12:18 +0200

    Oopsie

--- code/Q3/dataset.py
+++ code/Q3/dataset.py
@@ -1,3 +1,24 @@
+import re
+import torch as th
+
+ANS_RE = re.compile(r"#### (\-?[0-9\.\,]+)")
+INVALID_ANS = "[invalid]"
+
+
+def extract_answer(completion):
+    match = ANS_RE.search(completion)
+    if match:
+        match_str = match.group(1).strip()
+        match_str = match_str.replace(",", "")
+        return match_str
+    else:
+        return INVALID_ANS
+
+def is_correct(model_completion, gt_example):
+    gt_answer = extract_answer(gt_example["answer"])
+    assert gt_answer != INVALID_ANS
+    return extract_answer(model_completion) == gt_answer
+
 class GSMDataset(th.utils.data.Dataset):
     def __init__(self, tokenizer, ds, loss_on_prefix=True):
         self.qns = ds['question']
